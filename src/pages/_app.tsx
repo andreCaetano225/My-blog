b486506515fc7cfa1a674be45b5cf5823450270e
andreCaetano225 <andre.caetano2007@hotmail.com>
2022-05-14 15:03:51 -0300

    Adicionando mobile

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,16 @@
 import type { AppProps } from 'next/app'
 import '../styles/globals.scss'
 import { ThemeProvider } from 'next-themes'
+import Head from 'next/head'
 
 
 function MyApp({ Component, pageProps }: AppProps) {
 
   return (
     <ThemeProvider disableTransitionOnChange>
+      <Head>
+        <title>André Sousa - Portfolio</title>
+      </Head>
       <Component {...pageProps} />
     </ThemeProvider>
   )
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,7 +8,6 @@ export default function Document() {
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
                 <link rel="preconnect" href="https://fonts.gstatic.com" />
                 <link href="https://fonts.googleapis.com/css2?family=Sora:wght@100;300;400;500;600;700&display=swap" rel="stylesheet" />
-                <title>André Sousa - Portfolio</title>
             </Head>
             <body>
                 <Main />
