e288a39843dcd4d74fd5c1908e6ba9ecdc82288b
Andre Sousa <andre.sousa@shareprime.com.br>
2022-05-19 07:05:26 -0300

    Subindo aplicação em sua primeira versão

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,6 +8,7 @@ export default function Document() {
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
                 <link rel="preconnect" href="https://fonts.gstatic.com" />
                 <link href="https://fonts.googleapis.com/css2?family=Sora:wght@100;300;400;500;600;700&display=swap" rel="stylesheet" />
+                <link rel="shortcut icon" href="/favicon.svg" type="image/x-icon" />
             </Head>
             <body>
                 <Main />
